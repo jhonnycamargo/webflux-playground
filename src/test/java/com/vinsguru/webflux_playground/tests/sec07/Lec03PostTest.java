0bab967d90cd82781b50cd21efb50e1aeb8bd1f5
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-06 10:31:03 -0400

    POST - Body Publisher vs Body Value.

---
+++ src/test/java/com/vinsguru/webflux_playground/tests/sec07/Lec03PostTest.java
@@ -0,0 +1,45 @@
+package com.vinsguru.webflux_playground.tests.sec07;
+
+import com.vinsguru.webflux_playground.tests.sec07.dto.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class Lec03PostTest extends AbstractWebClient {
+
+    private final WebClient client = createWebClient();
+
+    @Test
+    public void postBodyValue() throws InterruptedException {
+        var product = new Product(null, "iphone", 1000);
+        this.client.post()
+                .uri("/lec03/product")
+                .bodyValue(product)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void postBody() throws InterruptedException {
+        var mono = Mono.fromSupplier(() -> new Product(null, "iphone", 1000))
+                .delayElement(Duration.ofSeconds(1));
+        this.client.post()
+                .uri("/lec03/product")
+                .body(mono, Product.class)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+}
