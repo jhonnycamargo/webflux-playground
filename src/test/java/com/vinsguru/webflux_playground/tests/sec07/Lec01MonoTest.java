33447423b04d6726bad3267550ad9a4d732d2ffb
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-06 09:38:55 -0400

    Non-blocking Concurrent Requests.

---
+++ src/test/java/com/vinsguru/webflux_playground/tests/sec07/Lec01MonoTest.java
@@ -0,0 +1,39 @@
+package com.vinsguru.webflux_playground.tests.sec07;
+
+import com.vinsguru.webflux_playground.tests.sec07.dto.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+public class Lec01MonoTest extends AbstractWebClient {
+
+    private final WebClient client = createWebClient();
+
+    @Test
+    public void simpleGet() throws InterruptedException {
+        this.client.get()
+                .uri("/lec01/product/1")
+                .retrieve()
+                .bodyToMono(Product.class)
+                .doOnNext(print())
+                .subscribe();
+
+        Thread.sleep(Duration.ofSeconds(2));
+    }
+
+    @Test
+    public void concurrentRequest() throws InterruptedException {
+        for (int i = 1; i <= 100; i++) {
+            this.client.get()
+                    .uri("/lec01/product/{id}", i)
+                    .retrieve()
+                    .bodyToMono(Product.class)
+                    .doOnNext(print())
+                    .subscribe();
+        }
+
+        Thread.sleep(Duration.ofSeconds(2));
+    }
+
+}
---
+++ src/test/java/com/vinsguru/webflux_playground/tests/sec07/dto/Product.java
@@ -0,0 +1,6 @@
+package com.vinsguru.webflux_playground.tests.sec07.dto;
+
+public record Product(Integer id,
+                      String description,
+                      Integer price) {
+}
