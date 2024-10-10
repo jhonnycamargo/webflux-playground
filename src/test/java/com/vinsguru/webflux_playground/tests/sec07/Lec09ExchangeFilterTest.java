78ae596049e2707403130a42eac7e5d30933b1ba
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-09 21:22:10 -0400

    Request attributes filter function.

--- src/test/java/com/vinsguru/webflux_playground/tests/sec07/Lec09ExchangeFilterTest.java
+++ src/test/java/com/vinsguru/webflux_playground/tests/sec07/Lec09ExchangeFilterTest.java
@@ -22,6 +22,7 @@ public class Lec09ExchangeFilterTest extends AbstractWebClient {
         for (int i = 1; i <= 5; i++) {
             this.client.get()
                     .uri("/lec09/product/{id}", i)
+                    .attribute("enable-logging", i % 2 == 0)
                     .retrieve()
                     .bodyToMono(Product.class)
                     .doOnNext(print())
@@ -46,7 +47,11 @@ public class Lec09ExchangeFilterTest extends AbstractWebClient {
 
     private ExchangeFilterFunction requestLogger() {
         return (clientRequest, next) -> {
-            log.info("Request url - {}: {}", clientRequest.method(), clientRequest.url());
+            var isEnabled = (Boolean) clientRequest.attributes().getOrDefault("enable-logging", false);
+            if (isEnabled) {
+                log.info("Request url - {}: {}", clientRequest.method(), clientRequest.url());
+            }
+
             return next.exchange(clientRequest);
         };
     }
