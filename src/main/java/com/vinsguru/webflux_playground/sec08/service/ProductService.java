e4d48f016874b0dca9916a0794efdfce47a9dbb9
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-10 19:46:40 -0400

    Added product service.

---
+++ src/main/java/com/vinsguru/webflux_playground/sec08/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.vinsguru.webflux_playground.sec08.service;
+
+import com.vinsguru.webflux_playground.sec08.dto.ProductDto;
+import com.vinsguru.webflux_playground.sec08.mapper.EntityDtoMapper;
+import com.vinsguru.webflux_playground.sec08.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Flux<ProductDto> saveProducts(Flux<ProductDto> productDtoFlux){
+        return productDtoFlux
+                .map(EntityDtoMapper::toEntity)
+                .as(productRepository::saveAll)
+                .map(EntityDtoMapper::toDto);
+    }
+
+    public Mono<Long> getProductsCount(){
+        return productRepository.count();
+    }
+}
