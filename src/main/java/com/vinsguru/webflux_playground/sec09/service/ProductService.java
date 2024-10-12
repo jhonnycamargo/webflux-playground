64cdf002147f50d5d5b24b153ccac92ace24a7f6
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-12 09:43:35 -0400

    Emitting Items via Sink.

--- src/main/java/com/vinsguru/webflux_playground/sec09/service/ProductService.java
+++ src/main/java/com/vinsguru/webflux_playground/sec09/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 @Service
 public class ProductService {
@@ -14,19 +15,19 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public Flux<ProductDto> saveProducts(Flux<ProductDto> productDtoFlux){
-        return productDtoFlux
+    @Autowired
+    private Sinks.Many<ProductDto> sink;
+
+    public Mono<ProductDto> saveProducts(Mono<ProductDto> productDtoMono){
+        return productDtoMono
                 .map(EntityDtoMapper::toEntity)
-                .as(productRepository::saveAll)
-                .map(EntityDtoMapper::toDto);
+                .flatMap(this.productRepository::save)
+                .map(EntityDtoMapper::toDto)
+                .doOnNext(sink::tryEmitNext);
     }
 
-    public Mono<Long> getProductsCount(){
-        return productRepository.count();
+    public Flux<ProductDto> productStream(){
+        return this.sink.asFlux();
     }
 
-    public Flux<ProductDto> allProducts(){
-        return productRepository.findAll()
-                .map(EntityDtoMapper::toDto);
-    }
 }
